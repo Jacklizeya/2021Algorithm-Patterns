76480fe52b4f795e0bd7c51793ecc4ee5f2051e4
Jack Zeya Li <74479553+Jacklizeya@users.noreply.github.com>
2021-10-09 21:14:23 -0600

    Update 20211009 Chapter2S9 Permutations.js

--- 20211009 Chapter2S9 Permutations.js
+++ 20211009 Chapter2S9 Permutations.js
@@ -29,3 +29,42 @@ function convertToObject(string) {
   }
   return storage
 }
+
+
+// An even better solution
+
+const find_permutation = function(str, pattern) {
+  // TODO: Write your code here
+  let storage = {}
+  for (let char of pattern) {
+    if (!storage[char]) {storage[char] = 1} else {storage[char]++}
+  }
+
+  let endIndex = -1
+  let startIndex = 0
+  let matched = 0
+
+  while (endIndex < str.length) { 
+    endIndex++
+    let character = str[endIndex]
+    if (storage[character] !== undefined) {storage[character]--}
+    if (storage[character] === 0) {matched++}
+
+    if (endIndex - startIndex + 1 > pattern.length) {
+      character = str[startIndex]
+      if (storage[character] === 0) {matched--}
+      if (storage[character]) {storage[character]++}
+      startIndex++
+    }
+
+    if (matched === Object.keys(storage).length) {return true}
+  }
+
+  return false;
+};
+
+let String="oidbcaf", Pattern="abc"
+console.log(find_permutation(String, Pattern))
+
+
+
